4d90a8c8bc54089d37aca1812b723027e8f38a4b
JonaaaDev <jonathangimenezdev@gmail.com>
2025-12-04 03:14:03 +0100

    Todos los valores de c++

---
+++ Variables.cpp
@@ -0,0 +1,39 @@
+// int
+int myInt = 1;
+
+// float
+float myFloat = 1.0F;
+
+// double
+double myDouble = 1.0;
+
+// char
+char myChar = 'A';
+
+// bool
+bool myBool = true;
+
+// string
+#include <string>
+std::string myString = "Hello";
+
+// long
+long myLong = 123456789L;
+
+// short
+short myShort = 123;
+
+// unsigned int
+unsigned int myUnsignedInt = 100;
+
+// long long
+long long myLongLong = 123456789012345LL;
+
+// unsigned long
+unsigned long myUnsignedLong = 123456789UL;
+
+// unsigned short
+unsigned short myUnsignedShort = 100;
+
+// unsigned char
+unsigned char myUnsignedChar = 'B';
